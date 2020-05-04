63abd8094b23e2ba2897f88d1cc1deec70bacfd6
rayhanhemel <rayhanhemel@gmail.com>
2020-05-04 23:12:02 +0600

    Database Connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express()
+
+
+mongoose.connect(process.env.DATABASE, { useUnifiedTopology: true, useNewUrlParser: true }, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to the Database");
+    }
+});
 
 //Middlewares
 app.use(morgan('dev'));
@@ -11,7 +24,7 @@ app.use(bodyParser.urlencoded({extended: false }));
 
 //Get - Retieve data from the server
 app.get("/", (req, res) => {
-    res.json("Hello Worls Kutiba")
+    res.json("Hello World Kutiba")
 });
 
 //POST - Sent data from frontent to backend 
